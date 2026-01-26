8ad8aa732afb1f291d6b214983a09ba09cbbe118
Jose Valduz <jose.valduz@unet.edu.ve>
2026-01-26 14:17:13 -0400

    first commit

---
+++ variables/variables.py
@@ -0,0 +1,14 @@
+a = 5
+b = 8
+c=a+b
+print (c)
+
+nombre = "Alejandro"
+
+#concatenar con f-string
+bienvenida = f"Hola {nombre} como estas?"
+print(bienvenida)
+
+#operadores de pertenencia (in / not in)
+print("Lucas" in bienvenida)
+print("Lucas" not in bienvenida)
