fcdf1d53db651a86215992635e991ba0d4698af7
Jose Valduz <jose.valduz@unet.edu.ve>
2026-01-26 14:38:15 -0400

    snake_case

--- variables/variables.py
+++ variables/variables.py
@@ -3,12 +3,22 @@ b = 8
 c=a+b
 print (c)
 
+#Definicion de Variable
 nombre = "Alejandro"
+#DefinicionConCamelCase
+nombreCompletoTuTioMaster = "Luca Dalto"
+
+#Definiendo con snake_case
+nombre_completo_tu_tio_master = "Luca Dalto"
+
+#concatenar con +
+bienvenida = f"Hola"+ " como estas?"
 
 #concatenar con f-string
 bienvenida = f"Hola {nombre} como estas?"
-print(bienvenida)
+
 
 #operadores de pertenencia (in / not in)
 print("Lucas" in bienvenida)
 print("Lucas" not in bienvenida)
+
