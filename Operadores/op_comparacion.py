a2c4fad38b2047cca468debdd40231473ed031fe
Jose Valduz <jose.valduz@unet.edu.ve>
2026-01-27 11:51:47 -0400

    Operadores de comparacion
    ==
    !=
    <
    >
    <=
    >=

---
+++ Operadores/op_comparacion.py
@@ -0,0 +1,20 @@
+#Operadores de Comparacion
+igual_que = 5 == 6 #Devuelven true o false
+diferente_que = 5 != 6
+mayor_que = 5 > 6
+menor_que = 5 < 6
+mayor_o_igual_que = 5 >= 6
+menor_o_igual_que = 5 <= 6
+
+print(menor_o_igual_que)
+
+a=5
+b=10
+c=20
+comparacion = a+b < c 
+print(comparacion)
+
+#comparar usuarios
+usuario_de_base_de_datos = "admin"
+usuario_escrito = "Admin"
+print(usuario_de_base_de_datos == usuario_escrito)
