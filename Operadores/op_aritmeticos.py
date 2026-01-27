1e2167041837d6e794cef3f2eff4a9f9b104642d
Jose Valduz <jose.valduz@unet.edu.ve>
2026-01-27 11:39:15 -0400

    Operadores Aritmeticos suma resta multiplicacion division divison entera resto potencia

---
+++ Operadores/op_aritmeticos.py
@@ -0,0 +1,19 @@
+#suma y resta (+ , -)
+suma = 10 + 5
+resta = 10 - 5
+
+#multiplicacion (* , **)
+multiplicacion = 10 * 5
+
+#division (/ , //)
+division = 10 / 5 #devuelve un float
+division_entera = 10 // 5 #devuelve un entero redondeando hacia abajo
+
+#modulo (%)
+resto = 10 % 3 #devuelve el resto de la division
+#potencia (**)
+potencia = 10 ** 5
+
+tipo_de_dato = type(division_entera) #devuelve el tipo de dato
+
+print(tipo_de_dato)
