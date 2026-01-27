577c76bf67d70d71d7d7e392c616d29d906cd5ff
Jose Valduz <jose.valduz@unet.edu.ve>
2026-01-27 12:30:51 -0400

    Operadores Logicos AND & OR | NOT not

---
+++ Operadores/op_logicos.py
@@ -0,0 +1,17 @@
+#AND
+Resultado = True & True #Devuelve True 
+Resultado2 = True & False #Devuelve False
+Resultado3 = False & False #Devuelve False 
+Resultado4 = False & True #Devuelve False
+
+#OR
+Resultado5 = True | True #Devuelve True
+Resultado6 = True | False #Devuelve True
+Resultado7 = False | False #Devuelve False
+Resultado8 = False | True #Devuelve True  
+
+#NOT
+Resultado9 = not True #Devuelve False
+Resultado10 = not False #Devuelve True  
+
+print(Resultado)
