0d532d96bd610ee375ed27f62c3216681eae13c3
Jose Valduz <jose.valduz@unet.edu.ve>
2026-01-30 14:04:38 -0400

    Agregar scripts para solicitar y mostrar datos del usuario uso del casteo

---
+++ inputs/input_nombres.py
@@ -0,0 +1,6 @@
+#Pedirle un dato al usuario
+nombre = input("¿Cuál es tu nombre?: ")
+
+#Mostrando el dato ingresado por el usuario
+print(f'Hola {nombre} , bienvenido al curso de Python.')
+
---
+++ inputs/input_numeros.py
@@ -0,0 +1,11 @@
+#pedirle un numero al usuario
+numero = input("Por favor ingresa un número: ")
+
+#multiplicando el numero por 2
+#resultado_entero = int(numero) * 2 #Se convierte a numero con int()
+
+#convierto el numero a flotante y multiplico *2
+resultado_flotante = float(numero) * 2
+
+#mostrando el resultado
+print(f'El resultado de multiplicar {numero} por 2 es: {resultado_flotante}')
