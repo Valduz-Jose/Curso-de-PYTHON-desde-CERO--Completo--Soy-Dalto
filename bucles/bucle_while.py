a562bd748ff4a3a8b63c90d95fd489006de4708b
Jose Valduz <jose.valduz@unet.edu.ve>
2026-01-30 18:46:02 -0400

    Agregar ejemplo de bucle while en Python

---
+++ bucles/bucle_while.py
@@ -0,0 +1,10 @@
+contador = 0
+
+#Mientras que la condicion sea verdadera, se ejecuta el bucle se va a seguir ejecutando
+
+while contador < 10:
+    contador += 1  # Incremento para evitar bucle infinito
+    print(contador) #Hasta aqui seria infinito
+
+print("El while ha terminado")
+
