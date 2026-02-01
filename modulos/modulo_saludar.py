b6a4e927a6dbefaff578e58d6c5622e1ce73f3b1
Jose Valduz <jose.valduz@unet.edu.ve>
2026-02-01 11:57:09 -0400

    Agregar funciones de saludo y su uso en el módulo principal

---
+++ modulos/modulo_saludar.py
@@ -0,0 +1,6 @@
+def saludar(nombre):
+    return f"¡Hola, {nombre}! Espero que tengas un gran día."
+def saludar_raro(nombre):
+    return f"¡Hola, {nombre}! QUEEEEE ONDAAAAA."
+
+# print(saludar("Carlos"))
---
+++ modulos/modulos.py
@@ -0,0 +1,6 @@
+# import modulo_saludar as m_saludar #el as sirve para renombrar el modulo al importarlo
+#para importar solo la funcion saludar del modulo_saludar.py
+from modulo_saludar import saludar,saludar_raro as S_Loco
+print(saludar("Ana"))
+print(S_Loco("Pedro"))
+
