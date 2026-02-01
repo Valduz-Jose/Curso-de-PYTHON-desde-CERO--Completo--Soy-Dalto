ad8256751c3edf9ed16d838bb3bef84c82393a60
Jose Valduz <jose.valduz@unet.edu.ve>
2026-02-01 13:28:05 -0400

    Agregar módulo de saludo con funciones para saludar y saludar raro

---
+++ modulos/paquete/saludar.py
@@ -0,0 +1,9 @@
+#Funciones
+def saludar(nombre):
+    return f"¡Hola, {nombre}! Espero que tengas un gran día."
+
+  
+# print(__name__)
+#variables
+# saludo = "Sendo Loco"
+# print(saludar("Carlos"))
---
+++ modulos/paquete/saludar_raro.py
@@ -0,0 +1,8 @@
+
+def saludar_raro(nombre):
+    return f"¡Hola, {nombre}! QUEEEEE ONDAAAAA."
+  
+# print(__name__)
+#variables
+# saludo = "Sendo Loco"
+# print(saludar("Carlos"))
